56b56d34e2eaf5405006282ccc8b841e6bf80de9
Moumou <moumou@helb.com>
2022-07-13 18:38:05 +0200

    first commit

---
+++ Frogger.java
@@ -0,0 +1,37 @@
+import java.awt.EventQueue;
+import java.io.IOException;
+import javax.swing.JFrame;
+
+public class Frogger extends JFrame {
+
+    public Frogger() throws IOException {
+        
+        initUI();
+    }
+    
+    private void initUI() throws IOException {
+        
+        add(new Board());
+               
+        setResizable(false);
+        pack();
+        
+        setTitle("Frogger");
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+    
+
+    public static void main(String[] args) {
+        
+        EventQueue.invokeLater(() -> {
+            JFrame ex = null;
+            try {
+                ex = new Frogger();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            ex.setVisible(true);
+        });
+    }
+}
