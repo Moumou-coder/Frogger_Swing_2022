e9b64a19492602707aef977c2c6c34af2daa0b6c
Moumou <moumou@pop-os.localdomain>
2022-07-26 21:20:39 +0200

    fix bug

--- Frog.java
+++ Frog.java
@@ -1,12 +1,55 @@
-public class Frog extends GameElement {
+import javax.swing.*;
+import java.awt.*;
+import java.io.ByteArrayInputStream;
 
+public class Frog {
+
+    private int pos_x;
+    private int pos_y;
+    private int speed;
+    private Image iconImage;
     private int numberOfLife;
     private boolean superFrog;
 
-    public Frog(int pos_x, int pos_y, int speed, int numberOfLife, boolean superFrog) {
-        super(pos_x, pos_y, speed, "./assets/head.png");
+    public Frog() {
+        this.pos_x = Board.B_WIDTH/2;
+        this.pos_y = Board.B_HEIGHT-25;
+        this.speed = Board.DOT_SIZE;
         this.numberOfLife = 3;
         this.superFrog = false;
+        this.iconImage = new ImageIcon("./assets/head.png").getImage();
+    }
+
+    public int getPos_x() {
+        return pos_x;
+    }
+
+    public void setPos_x(int pos_x) {
+        this.pos_x = pos_x;
+    }
+
+    public int getPos_y() {
+        return pos_y;
+    }
+
+    public void setPos_y(int pos_y) {
+        this.pos_y = pos_y;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public Image getIconImage() {
+        return iconImage;
+    }
+
+    public void setIconImage(Image iconImage) {
+        this.iconImage = iconImage;
     }
 
     public int getNumberOfLife() {
@@ -25,8 +68,4 @@ public class Frog extends GameElement {
         this.superFrog = superFrog;
     }
 
-    @Override
-    public void triggerAction(Board board) {
-
-    }
 }
